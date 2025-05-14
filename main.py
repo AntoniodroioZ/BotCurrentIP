2bba05727f5d2171c97ca4f16722da26bcad337d
antoniodroios <antonybey13@gmail.com>
2025-05-13 21:24:24 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,141 @@
+import os
+import requests
+import time
+import socket
+from dotenv import load_dotenv
+from telegram import Update
+from telegram import Update
+from telegram.ext import Application, CommandHandler, CallbackContext
+
+# Cargar las variables de entorno del archivo .env
+load_dotenv()
+# Obtener las variables de entorno
+TOKEN = os.getenv("TOKEN")
+IP_LOCAL = os.getenv("IP_LOCAL")
+CHAT_ID = os.getenv("CHAT_ID")
+USE_LOCAL = os.getenv("USE_LOCAL", "False")  # Default a "False" si no está definida
+
+# Parsea valores
+CHAT_ID = int(CHAT_ID)
+USE_LOCAL = USE_LOCAL.lower() == "true"
+
+# Ahora USE_LOCAL será un valor booleano
+print(USE_LOCAL)
+
+# Obtener la IP pública
+def obtener_ip():
+    return requests.get("https://api.ipify.org").text
+
+# Comando para enviar la IP actual
+async def ip(update: Update, context: CallbackContext):
+    ip = obtener_ip()
+    await update.message.reply_text(f"Tu IP actual es: {ip}")
+
+# Enviar la IP de manera automática cada día
+async def enviar_ip_diaria(context: CallbackContext):
+    ip = obtener_ip()
+    chat_id = context.job.context['chat_id']
+    await context.bot.send_message(chat_id=chat_id, text=f"IP diaria actualizada: {ip+":25565"}")
+
+# Comando /start
+async def start(update: Update, context: CallbackContext):
+    user = update.message.from_user
+    username = user.username
+    print(username)
+
+    welcome_message = f"""
+    ¡Hola {username}! Soy tu bot del servidor de Minecraft. 😄
+    Mi función principal es proporcionarte la IP actualizada de tu servidor y verificar su estado. Aquí tienes una lista de los comandos disponibles:
+
+    🔹 **/ip**  
+    Obtén la **IP actualizada** de tu servidor de Minecraft.
+
+    🔹 **/checkServer**  
+    Verifica el **estado del servidor** y te indicaré si está activo o no.
+
+    🌍 Estoy aquí para ayudarte a mantener todo funcionando correctamente. Si necesitas algo más, no dudes en preguntar. ¡Que disfrutes jugando! 🎮
+    """
+    
+    await update.message.reply_text(welcome_message, parse_mode="Markdown")
+
+
+    # context.job_queue.run_daily(enviar_ip_diaria, time=time.time(), context={'chat_id': chat_id})
+
+async def check_server(update: Update, context: CallbackContext):
+    ip_servidor = ""
+    if USE_LOCAL:
+        ip_servidor = IP_LOCAL 
+        print(ip_servidor)
+    else:
+        ip_servidor = obtener_ip()
+        print(ip_servidor)
+        
+    puerto = 25566
+
+    try:
+        # Intentar conectarse al servidor
+        with socket.create_connection((ip_servidor, puerto), timeout=5):
+            status_message = "¡El servidor de Minecraft está activo y funcionando en el puerto 25566!"
+    except (socket.timeout, socket.error):
+        status_message = "El servidor de Minecraft no está respondiendo en el puerto 25566. Puede que esté apagado o haya un problema de red, notifica al mediocre del administrador con: /notifyStatus"
+
+    # Enviar el mensaje de estado
+    await update.message.reply_text(status_message)
+
+# Función para enviar el estado a un usuario específico
+async def notify_status(update: Update, context: CallbackContext):
+    ip_servidor = ""
+    if USE_LOCAL:
+        ip_servidor = IP_LOCAL 
+        print(ip_servidor)
+    else:
+        ip_servidor = obtener_ip()
+        print(ip_servidor)
+    puerto = 25566
+
+    try:
+        # Intentar conectarse al servidor
+        with socket.create_connection((ip_servidor, puerto), timeout=5):
+            status_message = "¡El servidor de Minecraft está activo y funcionando en el puerto 25566!"
+    except (socket.timeout, socket.error):
+        status_message = "El servidor de Minecraft no está respondiendo en el puerto 25566. Puede que esté apagado o haya un problema de red."
+
+    # Enviar el mensaje a un chat_id específico
+    await context.bot.send_message(chat_id=CHAT_ID, text=status_message)
+    await update.message.reply_text("Notificación enviada al administrador mediocre correctamente.")
+    
+async def jourada(update: Update, context: CallbackContext):
+    # Ruta del archivo de la imagen
+    image_path = "img/sticker.webp"
+    
+    # Enviar la imagen al usuario
+    await update.message.reply_photo(photo=open(image_path, 'rb'))
+ 
+
+def main():
+
+
+    # Crea la aplicación
+    application = Application.builder().token(TOKEN).build()
+
+    # Registra los comandos
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("ip", ip))
+    application.add_handler(CommandHandler("checkServer", check_server))
+    application.add_handler(CommandHandler("notifyStatus", notify_status))
+    application.add_handler(CommandHandler("jourada", jourada))
+
+
+    # Inicia el bot
+    application.run_polling()
+    
+    if KeyboardInterrupt:
+        print("\Bot detenido por el usuario")
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
